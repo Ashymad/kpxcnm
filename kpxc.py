3cb8b4706f62511d911d734301152475fffd59dc
Ashymad <czilukim@o2.pl>
2018-03-10 18:52:41 +0100

    Reading encrypted messages works

--- kpxc.py
+++ kpxc.py
@@ -1,6 +1,6 @@
 import nacl.utils
-import nacl.encoding
-from nacl.public import PrivateKey, Box
+from nacl import encoding
+from nacl.public import PrivateKey, PublicKey, Box
 
 import json
 import base64 as b64
@@ -12,11 +12,21 @@ class kpxc:
     CLIENT_ID_SIZE = 24
     NONCE_SIZE = 24
     def __init__(self):
-        self.client_id = b64.b64encode(
-            nacl.utils.random(self.CLIENT_ID_SIZE)).decode('UTF-8')
+        self.client_id = self._to_b64_str(
+            nacl.utils.random(self.CLIENT_ID_SIZE))
         self.privkey = PrivateKey.generate()
-        self.kpxc_proxy = Popen('keepassxc-proxy', stdout=PIPE, stdin=PIPE,
-                                stderr=STDOUT);
+        self.pubkey = self.privkey.public_key.encode(
+                            encoding.Base64Encoder).decode('UTF-8')
+        self.kpxc_proxy = Popen('keepassxc-proxy', stdout=PIPE,
+                                stdin=PIPE, stderr=STDOUT);
+    @staticmethod
+    def _to_b64_str(bytedata):
+        return b64.b64encode(bytedata).decode('UTF-8')
+    @staticmethod
+    def _from_b64_str(bytedata):
+        return b64.b64decode(bytedata.encode('UTF-8'))
+    def _get_nonce(self):
+        return self._to_b64_str(nacl.utils.random(self.NONCE_SIZE))
     def _send_message(self, message):
         message = json.dumps(message).encode('UTF-8')
         self.kpxc_proxy.stdin.write(struct.pack('I',len(message)))
@@ -26,17 +36,43 @@ class kpxc:
         txt_len_b = self.kpxc_proxy.stdout.read(4);
         if txt_len_b == 0: return
         txt_len = struct.unpack('i', txt_len_b)[0]
-        return self.kpxc_proxy.stdout.read(txt_len).decode('UTF-8')
+        return json.loads(
+            self.kpxc_proxy.stdout.read(txt_len).decode('UTF-8'))
+    def _send_encrypted_message(self, message):
+        nonce = nacl.utils.random(self.NONCE_SIZE)
+        self._send_message({
+            'action'    : message['action'],
+            'message'   : self._to_b64_str(self.kp_box.encrypt(
+                    json.dumps(message).encode('UTF-8'), nonce)),
+            'nonce'     : self._to_b64_str(nonce),
+            'clientID'  : self.client_id
+        })
+    def _read_encrypted_message(self):
+        message = self._read_message()
+        return json.loads(self.kp_box.decrypt(
+            self._from_b64_str(message['message']),
+            self._from_b64_str(message['nonce'])).decode('UTF-8'))
     def change_public_keys(self):
         self._send_message({
-            'action' : 'change-public-keys',
-            'publicKey' : self.privkey.public_key.encode(
-                nacl.encoding.Base64Encoder).decode('UTF-8'),
-            'nonce' : b64.b64encode(
-                nacl.utils.random(self.NONCE_SIZE)).decode('UTF-8'),
-            'clientID' : self.client_id
+            'action'    : 'change-public-keys',
+            'publicKey' : self.pubkey,
+            'nonce'     : self._get_nonce(),
+            'clientID'  : self.client_id
         })
-        print(self._read_message())
-
-
-
+        response = self._read_message()
+        is_success = response['success'] == 'true'
+        if is_success:
+            self.kp_key = PublicKey(
+                response['publicKey'].encode('UTF-8'),
+                encoding.Base64Encoder)
+            self.kp_box = Box(self.privkey, self.kp_key)
+        return is_success
+    def get_databasehash(self):
+        self._send_encrypted_message({
+            'action'    : 'get-databasehash'
+        })
+    def associate(self):
+        self._send_encrypted_message({
+            'action'    : 'associate',
+            'key'       : self.pubkey
+        })
