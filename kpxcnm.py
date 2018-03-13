a8da3a1ace3d88ddf8f3d89985f1a0960d5f9a92
Ashymad <czilukim@o2.pl>
2018-03-13 23:19:46 +0100

    Type annotations

--- kpxcnm.py
+++ kpxcnm.py
@@ -6,6 +6,7 @@ import json
 import base64 as b64
 from subprocess import Popen, PIPE, STDOUT
 import struct
+from typing import Dict, List
 
 class KeePassError(Exception):
     def __init__(self, error_code : int, message : str):
@@ -24,19 +25,19 @@ class Kpxcnm:
         self.kpxc_proxy = Popen('keepassxc-proxy', stdout=PIPE,
                                 stdin=PIPE, stderr=STDOUT);
     @staticmethod
-    def _to_b64_str(bytedata : bytes):
+    def _to_b64_str(bytedata : bytes) -> str:
         return b64.b64encode(bytedata).decode('UTF-8')
     @staticmethod
-    def _from_b64_str(string : str):
+    def _from_b64_str(string : str) -> bytes:
         return b64.b64decode(string.encode('UTF-8'))
-    def _gen_nonce(self):
+    def _gen_nonce(self) -> str:
         return self._to_b64_str(nacl.utils.random(self.NONCE_SIZE))
-    def _send_message(self, message):
+    def _send_message(self, message) -> None:
         message = json.dumps(message).encode('UTF-8')
         self.kpxc_proxy.stdin.write(struct.pack('I',len(message)))
         self.kpxc_proxy.stdin.write(message)
         self.kpxc_proxy.stdin.flush()
-    def _read_message(self):
+    def _read_message(self) -> Dict[str, str]:
         txt_len_b = self.kpxc_proxy.stdout.read(4);
         if txt_len_b == 0: return
         txt_len = struct.unpack('i', txt_len_b)[0]
@@ -45,7 +46,8 @@ class Kpxcnm:
         if 'error' in message:
             raise KeePassError(int(message['errorCode']), message['error'])
         return message
-    def _send_encrypted_message(self, message : str, triggerUnlock : bool = False):
+    def _send_encrypted_message(self, message : Dict[str, str],
+                                triggerUnlock : bool = False) -> None:
         nonce = nacl.utils.random(self.NONCE_SIZE)
         self._send_message({
             'action'        : message['action'],
@@ -55,13 +57,13 @@ class Kpxcnm:
             'clientID'      : self.client_id,
             'triggerUnlock' : 'true' if triggerUnlock else 'false'
         })
-    def _read_encrypted_message(self):
+    def _read_encrypted_message(self) -> Dict[str,str]:
         message = self._read_message()
         return json.loads(self.kp_box.decrypt(
             self._from_b64_str(message['message']),
             self._from_b64_str(message['nonce'])).decode('UTF-8'))
         return message
-    def change_public_keys(self):
+    def change_public_keys(self) -> bool:
         self._send_message({
             'action'    : 'change-public-keys',
             'publicKey' : self.pubkey,
@@ -77,14 +79,14 @@ class Kpxcnm:
             self.kp_box = Box(self.privkey, self.db_pubkey)
             return is_success
         return False
-    def get_databasehash(self):
+    def get_databasehash(self) -> str:
         self._send_encrypted_message({
             'action'    : 'get-databasehash'
         })
         response = self._read_encrypted_message()
         if response['success'] == 'true':
             return response['hash']
-    def associate(self):
+    def associate(self) -> bool:
         self._send_encrypted_message({
             'action'    : 'associate',
             'key'       : self.pubkey
@@ -94,7 +96,7 @@ class Kpxcnm:
         if is_success:
             self.db_id = response['id']
         return is_success
-    def generate_password(self):
+    def generate_password(self) -> str:
         self._send_message({
             'action'    : 'generate-password',
             'nonce'     : self._gen_nonce(),
@@ -103,7 +105,8 @@ class Kpxcnm:
         response = self._read_encrypted_message()
         if response['success'] == 'true':
             return response['entries'][0]['password']
-    def get_logins(self, url : str, submitUrl : str = None):
+    def get_logins(self, url : str,
+                   submitUrl : str = None) -> List[Dict[str, str]]:
         self._send_encrypted_message({
             'action'    : 'get-logins',
             'url'       : url,
@@ -112,7 +115,7 @@ class Kpxcnm:
         response = self._read_encrypted_message()
         if response['success'] == 'true':
             return response['entries']
-    def test_associate(self):
+    def test_associate(self) -> bool:
         self._send_encrypted_message({
             'action'    : 'test-associate',
             'id'        : self.db_id,
@@ -121,13 +124,15 @@ class Kpxcnm:
         })
         response = self._read_encrypted_message()
         return response['success'] == 'true'
-    def lock_database(self):
+    def lock_database(self) -> bool:
         nonce = self._gen_nonce()
         self._send_encrypted_message({
             'action'    : 'lock-database'
         })
         response = self._read_encrypted_message()
-    def set_login(self, username : str, password : str, url : str):
+        return True
+    def set_login(self, username : str,
+                  password : str, url : str) -> bool:
         self._send_encrypted_message({
             'action'    : 'set-login',
             'id'        : self.db_id,
